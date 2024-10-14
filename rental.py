daf9d3c327d1a099adfa389b7652ae983f75194f
Natcha Ranchan <natcha.ran@ku.th>
2024-10-14 10:35:59 +0700

    Move Method.

--- rental.py
+++ rental.py
@@ -1,23 +1,52 @@
+import logging
+
+from movie import Movie
+
+
 class Rental:
-    """
-    A rental of a movie by customer.
-    From Fowler's refactoring example.
+	"""
+	A rental of a movie by customer.
+	From Fowler's refactoring example.
 
-    A realistic Rental would have fields for the dates
-    that the movie was rented and returned, from which the
-    rental period is calculated.
-    For simplicity of this application only days_rented is recorded.
-    """
-    
-    def __init__(self, movie, days_rented): 
-    	"""Initialize a new movie rental object for
-    	   a movie with known rental period (daysRented).
-    	"""
-    	self.movie = movie
-    	self.days_rented = days_rented
+	A realistic Rental would have fields for the dates
+	that the movie was rented and returned, from which the
+	rental period is calculated.
+	For simplicity of this application only days_rented is recorded.
+	"""
 
-    def get_movie(self):
-    	return self.movie
+	def __init__(self, movie, days_rented):
+		"""Initialize a new movie rental object for
+		   a movie with known rental period (daysRented).
+		"""
+		self.movie = movie
+		self.days_rented = days_rented
 
-    def get_days_rented(self):
-    	return self.days_rented
+	def get_movie(self):
+		return self.movie
+
+	def get_days_rented(self):
+		return self.days_rented
+
+	def get_price(self):
+		"""Calculate the price of rental.
+		Returns:
+			amount of rental
+		"""
+		amount = 0
+		if self.get_movie().get_price_code() == Movie.REGULAR:
+			# Two days for $2, additional days 1.50 per day.
+			amount = 2.0
+			if self.get_days_rented() > 2:
+				amount += 1.5 * (self.get_days_rented() - 2)
+		elif self.get_movie().get_price_code() == Movie.CHILDRENS:
+			# Three days for $1.50, additional days 1.50 per day.
+			amount = 1.5
+			if self.get_days_rented() > 3:
+				amount += 1.5 * (self.get_days_rented() - 3)
+		elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+			# Straight $3 per day charge
+			amount = 3 * self.get_days_rented()
+		else:
+			log = logging.getLogger()
+			log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
+		return amount
--- rental_test.py
+++ rental_test.py
@@ -5,26 +5,35 @@ from movie import Movie
 
 
 class RentalTest(unittest.TestCase):
-    
-    def setUp(self):
-    	self.new_movie = Movie("Dune: Part Two", Movie.NEW_RELEASE)
-    	self.regular_movie = Movie("Air", Movie.REGULAR)
-    	self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
 
-    def test_movie_attributes(self):
-    	"""trivial test to catch refactoring errors or change in API of Movie"""
-    	m = Movie("Air", Movie.REGULAR)
-    	self.assertEqual("Air", m.get_title())
-    	self.assertEqual(Movie.REGULAR, m.get_price_code())
+	def setUp(self):
+		self.new_movie = Movie("Dune: Part Two", Movie.NEW_RELEASE)
+		self.regular_movie = Movie("Air", Movie.REGULAR)
+		self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
 
-    @unittest.skip("add this test when you refactor rental price")
-    def test_rental_price(self):
-    	rental = Rental(self.new_movie, 1)
-    	self.assertEqual(rental.get_price(), 3.0)
-    	rental = Rental(self.new_movie, 5)
-    	self.assertEqual(rental.get_price(), 15.0)
-    	self.fail("TODO add more tests for other movie categories")
+	def test_movie_attributes(self):
+		"""trivial test to catch refactoring errors or change in API of Movie"""
+		m = Movie("Air", Movie.REGULAR)
+		self.assertEqual("Air", m.get_title())
+		self.assertEqual(Movie.REGULAR, m.get_price_code())
 
-    @unittest.skip("add this test of rental points when you add it to Rental")
-    def test_rental_points(self):
-    	self.fail("add this test of frequent renter points")
+	def test_rental_price(self):
+		"""Trivial test for rental price based on the movie type."""
+		rental = Rental(self.new_movie, 1)
+		self.assertEqual(rental.get_price(), 3.0)
+		rental = Rental(self.new_movie, 5)
+		self.assertEqual(rental.get_price(), 15.0)
+		# regular movie
+		rental = Rental(self.regular_movie, 2)
+		self.assertEqual(rental.get_price(), 2)
+		rental = Rental(self.regular_movie, 4)
+		self.assertEqual(rental.get_price(), 5)
+		# children movie
+		rental = Rental(self.childrens_movie, 3)
+		self.assertEqual(rental.get_price(), 1.5)
+		rental = Rental(self.childrens_movie, 6)
+		self.assertEqual(rental.get_price(), 6)
+
+	@unittest.skip("add this test of rental points when you add it to Rental")
+	def test_rental_points(self):
+		self.fail("add this test of frequent renter points")
